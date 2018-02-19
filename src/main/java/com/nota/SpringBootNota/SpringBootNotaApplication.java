2e2fda29bc23baab0d85dba4c8fe565f02ca34d1
jhfaber <jfmarina@unal.edu.co>
2018-02-19 15:24:50 -0500

    First commit

---
+++ src/main/java/com/nota/SpringBootNota/SpringBootNotaApplication.java
@@ -0,0 +1,12 @@
+package com.nota.SpringBootNota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNotaApplication.class, args);
+	}
+}
